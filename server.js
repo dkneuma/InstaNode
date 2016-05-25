5b7642a86bc61c1db16d725dd202820565b4946c
Daniel Neumann <dkneuma@gmail.com>
2016-05-24 21:33:28 -0400

    Added Instagram data

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ var express = require('express');
 var app = express();
 var ig = require('instagram-node').instagram();
 // CONFIGURE THE APP
+// configure instagram app with client_id
+ig.use({
+    client_id: 'd31786d346634699a0d497010fb04721',
+    client_secret: 'd7344a9ca01e4e67bfd250d6a2748214'
+});
+
 // ==================================================
 // tell node where to look for site resources
 app.use(express.static(__dirname + '/public'));
@@ -19,8 +25,11 @@ app.set('view engine', 'ejs');
 // home page route - popular images
 app.get('/', function(req, res) {
 // use the instagram package to get popular media
+// use the instagram package to get popular media
+    ig.media_popular(function(err, medias, remaining, limit) {
 // render the home page and pass in the popular images
-    res.render('pages/index');
+        res.render('pages/index', { grams: medias });
+    });
 });
 // START THE SERVER
 // ==================================================
